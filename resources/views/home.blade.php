8a8371b45e8f2768fd3325d49d5943ce458e007f
Muh Ghazali Akbar <muhghazaliakbar@live.com>
2019-01-07 13:33:00 +0800

    Update rules.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,8 @@
                     <h5 class="card-header"><i class="fas fa-exclamation-circle text-red"></i> Rules</h5>
 
                     <div class="card-body">
-                        test
+                        <p>Everyday, write down words as much as you can. But, the word must be started with last letter from previous word. The player MUST look the meaning for new word which the player never heard before. That’s the purpose of this game.</p>
+                        <p>Here’s an example : Mango - obligarchy - yoyo - orange - equal - lust - tame - elaborate and so on.</p>
                     </div>
                 </div>
             </div>
